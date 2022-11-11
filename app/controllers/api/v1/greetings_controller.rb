50e07f11c7cbc8c4c5129e193dffb6dcccc370be
myelin0 <emi.veisllari@gmail.com>
2022-11-11 08:59:55 +0100

    Api returns random json greeting

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::GreetingsController < ApplicationController
+  def index
+    random_greeting = Message.all.sample(1).first
+    render json: { status: 'SUCCESS', message: 'random greeting loaded', data: random_greeting.greeting }.to_json
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting_1 = Message.create(greeting: "Hello World!")
+greeting_2 = Message.create(greeting: "Ciao Mondo!")
+greeting_3 = Message.create(greeting: "Selam Dunya!")
+greeting_4 = Message.create(greeting: "Pershendetje Bote!")
+greeting_5 = Message.create(greeting: "Hola Mundo!")
